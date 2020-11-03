5dde3cace6ca8fced82879767dc1fe30c923b438
uimarshall <uimarshall@gmail.com>
2020-11-03 21:55:38 +0100

    Install and configure webpack

---
+++ src/app/app.js
@@ -0,0 +1,16 @@
+let doc
+doc = document
+doc = document.head
+doc = document.body
+doc = document.doctype
+doc = document.domain
+doc = document.documentURI
+doc = document.URL
+doc = document.contentType
+doc = document.characterSet
+doc = document.links
+doc = document.links[0].classList
+doc = document.links[0].className
+doc = document.images
+
+console.log(doc)
