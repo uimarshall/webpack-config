574c4ca724cd6ad600ea0160f4fc3c04d49b8e47
uimarshall <uimarshall@gmail.com>
2020-11-04 15:20:06 +0100

    Add configurations for cache bustings and plugins

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './main.scss'
+import './app/app'
 alert('Hello from webpack')
+console.log('Hi')
--- webpack.config.js
+++ webpack.config.js
@@ -12,16 +12,19 @@ module.exports = {
     }
 };*/
 // ***************************************************
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
     mode: "development",
     // devtool: "none",
     entry: "./src/index.js",
     output:{
-        filename:"main.js",//create helo.js in the path below
+        filename:"main.[contentHash].js",//create helo.js in the path below
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
     module: {
     rules: [
       {
